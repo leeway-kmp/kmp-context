1eebef1170a91dabcd99374400632347119452b5
abdulatif1707 <abdulatif1707@gmail.com>
2024-09-06 01:14:26 +0500

    initial commit

--- context/src/androidMain/kotlin/io/github/leeway/kmp/context/KmpContextInitializer.kt
+++ context/src/androidMain/kotlin/io/github/leeway/kmp/context/KmpContextInitializer.kt
@@ -3,6 +3,7 @@ package io.github.leeway.kmp.context
 import android.content.Context
 import androidx.startup.Initializer
 
+@Suppress("unused")
 class KmpContextInitializer : Initializer<Context> {
     override fun create(context: Context): Context {
         KmpContext.setUp(context)
